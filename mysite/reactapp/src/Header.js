1deb6101b11118c2853ceb8a1fb2713c38ca86d6
Douglas Fiedler <dognew@gmail.com>
2023-11-02 13:24:05 -0300

    Revert "Update front-end update"
    This reverts commit 54cadaf99b9b1fdb2d0eb376eebb1f24404a692f.

--- mysite/reactapp/src/Header.js
+++ mysite/reactapp/src/Header.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import minhaImagem from './Imagens/fundo.jpg'
 
 function Header() {
   return (
-    <header className="header">
-      <img
-        src={minhaImagem}
-        alt="Imagem de relaxamento"
-        className="header-image"
-      />
-      <h1 className="header-title">Burnout..E agora?</h1>
+    <header>
+      <h1>Síndrome de Burnout</h1>
     </header>
   );
 }
